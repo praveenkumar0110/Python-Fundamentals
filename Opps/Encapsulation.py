851668c33d0187ade4284c4df5c334ccaa3ec4cd
epraveenkumar7 <ipraveen.email@gmail.com>
2026-01-19 12:38:14 +0530

    Understand the encapsulated in one class

--- Opps/Encapsulation.py
+++ Opps/Encapsulation.py
@@ -31,3 +31,51 @@ class Student:
 s1 = Student(20)          # object created
 s1.set_age(100)           # age changed
 print(s1.get_age())  
+
+
+
+
+'''
+Without Encapsulation (Bad practice ❌)--------------------
+balance = 1000
+
+def withdraw(amount):
+    global balance
+    balance -= amount
+👉 Data & function separate
+👉 Any place-la balance change pannalaam 😬
+
+
+With Encapsulation (Correct 👍)-------------------------
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance   # private variable
+
+    def withdraw(self, amount):
+        self.__balance -= amount
+
+    def get_balance(self):
+        return self.__balance
+
+
+Usage:
+
+acc = BankAccount(1000)
+acc.withdraw(200)
+print(acc.get_balance())
+
+
+
+Neenga balance directly change panna mudiyadhu -------------summary most mportant
+
+ATM methods (withdraw, deposit) moolama dhaan
+
+👉 Idhu dhaan Encapsulation
+
+📌 One-line Thanglish summary
+
+Encapsulation = Data + Methods
+ore class-kulla irundhu
+unnecessary access-a hide pannradhu
+
+'''
--- Recursion/2types.py
+++ Recursion/2types.py
@@ -3,4 +3,12 @@ def count(n):
         return
     print(n)
     count(n - 1)      # RECURSIVE CASE
-count(5)
+count(6)
+
+# explaination
+# The function count is defined to take a single argument n.
+# It checks if n is 0 (the base case). If so, it returns without doing anything.
+# If n is not 0, it prints the value of n and then calls itself with n - 1 (the recursive case).
+# until n reaches 0, at which point the recursion stops.
+
+
